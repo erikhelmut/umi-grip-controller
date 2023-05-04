c74f5a2a7c1712b01698cc539010e18a4e0ad14c
Tim Schneider <tim.schneider94@t-online.de>
2023-05-04 13:42:00 +0200

    Increased version number to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="rhp12rn",
-    version="1.0.0",
+    version="1.0.1",
     description="Python controller for the ROBOTIS RH-P12-RN and RH-P12-RN(A) grippers.",
     long_description=long_description,
     long_description_content_type="text/markdown",
